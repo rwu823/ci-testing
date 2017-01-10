6ac8738a3de045f1792513037b79e90d35624af9
RoCky Wu <rocky823@gmail.com>
2017-01-10 21:50:47 +1100

    TEST_SUITE

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+import { version, repository } from '../package.json'
+import 'shelljs/global'
+
+const { TRAVIS_BRANCH, GH_TOKEN } = process.env
+console.log({ TRAVIS_BRANCH })
+
+const repoToken = repository.replace(/(github.com)/, `${GH_TOKEN}@$1`)
+const tag = `v${version}`
+
+if (TRAVIS_BRANCH === 'dev') {
+  console.log('deploying to aws......')
+
+  exec(`git tag ${tag}`)
+  exec(`git push ${repoToken} ${tag}`)
+}
