01dfd0dba2e64b0b85ecadcbdba8d4fd8a0bc54c
RoCky Wu <rocky823@gmail.com>
2017-01-14 13:19:17 +1100

    TRAVIS_PULL_REQUEST_BRANCH

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,11 @@
 const { version, repository } = require('../package.json')
 require('shelljs/global')
 
-const { TRAVIS_BRANCH, GH_TOKEN, TRAVIS_MATRIX } = process.env
+const { TRAVIS_BRANCH, GH_TOKEN, TRAVIS_MATRIX, TRAVIS_PULL_REQUEST_BRANCH } = process.env
 const repoToken = repository.replace(/(github.com)/, `${GH_TOKEN}@$1`)
 const tag = `v${version}`
 
-console.log({ TRAVIS_BRANCH, TRAVIS_MATRIX })
+console.log({ TRAVIS_BRANCH, TRAVIS_MATRIX, TRAVIS_PULL_REQUEST_BRANCH })
 
 if (TRAVIS_BRANCH === 'dev' && TRAVIS_MATRIX === 'build') {
   exec(`git config --global user.email "auto_deploy@circleci.com"`)
